2469388da5cc8fef5211a35523fdcbb58762689b
sakon <salanggo12@hotmail.com>
2019-12-15 01:19:05 +0700

    wagger-ui.html.

--- src/main/java/com/accountmanager/system/SwaggerConfig.java
+++ src/main/java/com/accountmanager/system/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.accountmanager.system;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
