bef0ef2823f0d1cdf3a0800e5f36e693f0b9f2e5
jook <kittiphong95@gmail.com>
2020-03-09 02:22:33 +0700

    journalSearch api group

---
+++ src/main/java/com/accountmanager/system/pojo/JournalSearchPojo.java
@@ -0,0 +1,45 @@
+package com.accountmanager.system.pojo;
+
+/**
+ * JournalSearchPojo
+ */
+public class JournalSearchPojo {
+
+    private String text;
+    private String passCode;
+    private String text2;
+    private String passCode2;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getPassCode() {
+        return passCode;
+    }
+
+    public void setPassCode(String passCode) {
+        this.passCode = passCode;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+    public String getPassCode2() {
+        return passCode2;
+    }
+
+    public void setPassCode2(String passCode2) {
+        this.passCode2 = passCode2;
+    }
+    
+}
